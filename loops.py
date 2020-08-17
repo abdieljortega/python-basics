8772e10f1e4f76757c0f53b81ac743abe086debd
Abdiel Ortega <nathanabdiel1@gmail.com>
2020-08-17 14:50:55 -0500

    The loop while was implemented

--- loops.py
+++ loops.py
@@ -1,13 +1,26 @@
-def loop(times = 100, count = 0):
-  if count > times:
-    return
-  else:
-    print(2 ** count)
-    count = count + 1
-    loop(times, count)
+# def loop(times = 100, count = 0):
+#   if count > times:
+#     return
+#   else:
+#     print(2 ** count)
+#     count = count + 1
+#     loop(times, count)
+
+# def run():
+#   loop()
+
+# if __name__ == '__main__':
+#   run()
 
 def run():
-  loop()
+  LIMIT = 1000
+
+  count = 0
+  powOf2 = 2 ** count
+  while powOf2 < LIMIT:
+    print('2 elevado a ' + str(count) + ' es igual a: ' + str(powOf2))
+    count = count + 1
+    powOf2 = 2 ** count
 
 if __name__ == '__main__':
   run()
