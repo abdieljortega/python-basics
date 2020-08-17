a31609c2f759777b5ca90d9dcca7adafc77593bd
Abdiel Ortega <nathanabdiel1@gmail.com>
2020-08-16 19:24:34 -0500

    The conditionals were implemented

---
+++ conditionals.py
@@ -0,0 +1,15 @@
+# age = int(input('Write your age: '))
+
+# if age >= 18:
+#   print('You are legal')
+# else:
+#   print('You\'re not legal')
+
+number = int(input('Write a number: '))
+
+if number > 5:
+  print('Is Grater than 5')
+elif number == 5:
+  print('Is equal to 5')
+else:
+  print('Is Smaller than 5')
