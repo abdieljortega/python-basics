2bd42a9312de435803c1a8bf753c8c36d2be572f
Abdiel Ortega <nathanabdiel1@gmail.com>
2020-08-17 15:27:02 -0500

    The for was implemented to a string

---
+++ goOver.py
@@ -0,0 +1,12 @@
+def run():
+  # name = input('Escribe tu nombre: ')
+  # for letter in name:
+  #   print(letter)
+
+  phrase = input('Escribe una frase: ')
+  for letter in phrase:
+    print(letter.upper())
+
+
+if __name__ == '__main__':
+  run()
